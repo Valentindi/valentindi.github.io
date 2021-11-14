33b410754211ca3fb2ec428993d6498e86ffe982
Valentin Dittmar <valentin.dittmar@datameer.com>
2021-11-14 10:22:31 +0100

    configure adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
-import preprocess from 'svelte-preprocess';
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: preprocess(),
-
+export default {
 	kit: {
-		target: '#svelte'
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+            appDir: '_app',
+		})
 	}
 };
-
-export default config;
